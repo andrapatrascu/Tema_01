c349b8c1e4a53cf7421ea413d669042d713cacc4
andrapatrascu <patrascuandra@yahoo.com>
2019-05-28 22:33:53 +0300

    Am rezolvat punctele 2 si 3 din tema.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Andra");
+        System.out.println("Suma lui 5 cu 3: " + (5 + 3));
+
+        int x = 6;
+        int y = 4;
+        int suma = x + y;
+        System.out.println("Suma lui 6 cu 4: " + suma);
+
+        float a = 10;
+        float b = 3;
+        System.out.println("Impartire: " + a/b);
     }
 }
